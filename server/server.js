d75b9d5e3e401a7b6e097e5a5d1d3cbfa7e1183e
CHITTESWARAN <chitteswarancj06@gmail.com>
2025-01-11 10:48:03 +0530

    the a project

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://student-management-app-1-frontend.onrender.com",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
