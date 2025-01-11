cf213d98f950dfecb23520cf251c76c510b7e243
CHITTESWARAN <chitteswarancj06@gmail.com>
2025-01-11 10:02:38 +0530

    Your commit message

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-export const axiosInstance=axios.create({
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:5001/api":"api",
-    withCredentials:true,
-})
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://student-management-app-ashen.vercel.app/api" 
+    : "api", 
+  withCredentials: true,
+});
