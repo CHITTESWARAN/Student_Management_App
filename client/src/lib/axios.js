e8a459c64e065705fefca0191148e3f6aeda45a0
CHITTESWARAN <chitteswarancj06@gmail.com>
2025-01-11 11:06:57 +0530

    Added build script to package.json

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" 
-        ? "http://localhost:5001/api" 
-        : "https://student-management-app-japo.onrender.com/api",
+    baseURL: "https://student-management-app-backend-614k.onrender.com",
     withCredentials: true,
 });
--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
   origin: [
-    "http://localhost:5173",
     "https://student-management-app-1-frontend.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
